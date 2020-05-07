e7da80af18afa788792c076c684c5ced9ea973d3
Ashwaq <ashkhmu@gmail.com>
2020-05-07 05:06:35 +0300

    Initial Commit

---
+++ day3Movie/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  day3Movie
+//
+//  Created by ash mutairi on 5/7/20.
+//  Copyright © 2020 Ashwaq. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
